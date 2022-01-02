baa2e54f4355e4437fd92e91c77bf88c33046dfc
oakmcilwain <oak.mcilwain@gmail.com>
2022-01-02 20:33:11 +0800

    initial commit

---
+++ exercises/move_semantics/move_semantics5.rs
@@ -0,0 +1,13 @@
+// move_semantics5.rs
+// Make me compile only be reordering the lines in `main()`, but without
+// adding, changing or removing any of them.
+// Execute `rustlings hint move_semantics5` for hints :)
+
+fn main() {
+    let mut x = 100;
+    let z = &mut x;
+    *z += 1000;
+    let y = &mut x;
+    *y += 100;
+    assert_eq!(x, 1200);
+}
